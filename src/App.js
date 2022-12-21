29d4ddee2f919a1168418619d08fdcd325d138a9
Rishabh Sahu <rishabhsahu@Rishabhs-MacBook-Air.local>
2022-12-22 01:09:20 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -10,6 +10,12 @@ function App() {
 
   let [filterValue, setFilterValue] = useState('all');
 
+  // TO SET FILTER VALUE
+  const filterHandler = (value) => {
+    setFilterValue(value);
+  };
+
+  // TO FILTER TODO
   let filterTodo = todo.filter((elem) => {
     if (filterValue === 'active') {
       return elem.isCompleted === false;
@@ -20,10 +26,7 @@ function App() {
     }
   });
 
-  const filterHandler = (value) => {
-    setFilterValue(value);
-  };
-
+  //TO ADD NEW TODO
   const addTodoHandler = (enteredText) => {
     setTodo((prevTodo) => {
       const updatedTodo = [...prevTodo];
@@ -37,6 +40,7 @@ function App() {
     setFilterValue('all');
   };
 
+  //TO DELETE TODO
   const deleteItemHandler = (todoId) => {
     setTodo((prevTodo) => {
       const updatedTodo = prevTodo.filter((todos) => todos.id !== todoId);
@@ -44,6 +48,7 @@ function App() {
     });
   };
 
+  //TO CLEAR COMPLETED TODO
   const clearCompletedHandlerFunction = () => {
     setTodo((prevTodo) => {
       const updatedTodo = prevTodo.filter(
@@ -53,6 +58,7 @@ function App() {
     });
   };
 
+  //TO COUNT THE NUMBER OF ACTIVE TODO
   const countNumberOfActiveTodo = () => {
     let activeCount = 0;
     todo.map((elem) => {
@@ -63,6 +69,7 @@ function App() {
     return activeCount;
   };
 
+  //TO COUNT THE NUMBER OF COMPLETED TODO
   const countNumberOfCompletedTodo = () => {
     let completedCount = false;
     todo.map((elem) => {
@@ -73,6 +80,7 @@ function App() {
     return completedCount;
   };
 
+  //TO TOGGLE THE STATUS
   const toggleStatus = (id) => {
     let newTodo = todo.map((task) => {
       if (task.id === id) {
@@ -83,6 +91,7 @@ function App() {
     setTodo(newTodo);
   };
 
+  //TO UPDATE THE STATUS
   const updateStatusHandler = () => {
     let flag = true;
     let newTodo = todo.map((task) => {
